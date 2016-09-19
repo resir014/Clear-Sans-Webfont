f4138e18ad6972d71f6b71a532db4abbc6259e64
Resi Respati <resir014@gmail.com>
2016-09-19 12:01:50 +0700

    [docs] disabled cssnano, recompressed css output

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,8 +23,7 @@ module.exports = function(grunt) {
     postcss: {
       options: {
         processors: [
-          require('autoprefixer')({ browsers: ['last 2 versions', 'ie 8', 'ie 9'] }),
-          require('cssnano')()
+          require('autoprefixer')({ browsers: ['last 2 versions', 'ie 8', 'ie 9'] })
         ]
       },
       dist: {
