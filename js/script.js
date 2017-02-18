e6d01ec8a9a42eed077510963f84d941a55f1f96
Resi Respati <resir014@gmail.com>
2017-02-19 01:51:48 +0700

    Built clear-sans-webfont from commit 6734d62 on branch master

--- js/script.js
+++ js/script.js
@@ -1,50 +1,52 @@
-$(document).ready(function() {
-  $('#btnThin').click(function(){
+/* eslint-env jquery */
+
+$(document).ready(function () {
+  $('#btnThin').click(function () {
     $('#specimen-test p').css({
       'font-weight': '100',
-      'font-style': 'normal',
-    });
-  });
-  $('#btnLight').click(function(){
+      'font-style': 'normal'
+    })
+  })
+  $('#btnLight').click(function () {
     $('#specimen-test p').css({
       'font-weight': '300',
-      'font-style': 'normal',
-    });
-  });
-  $('#btnRegular').click(function(){
+      'font-style': 'normal'
+    })
+  })
+  $('#btnRegular').click(function () {
     $('#specimen-test p').css({
       'font-weight': '400',
-      'font-style': 'normal',
-    });
-  });
-  $('#btnItalic').click(function(){
+      'font-style': 'normal'
+    })
+  })
+  $('#btnItalic').click(function () {
     $('#specimen-test p').css({
       'font-weight': '400',
-      'font-style': 'italic',
-    });
-  });
-  $('#btnMedium').click(function(){
+      'font-style': 'italic'
+    })
+  })
+  $('#btnMedium').click(function () {
     $('#specimen-test p').css({
       'font-weight': '500',
-      'font-style': 'normal',
-    });
-  });
-  $('#btnMediumItalic').click(function(){
+      'font-style': 'normal'
+    })
+  })
+  $('#btnMediumItalic').click(function () {
     $('#specimen-test p').css({
       'font-weight': '500',
-      'font-style': 'italic',
-    });
-  });
-  $('#btnBold').click(function(){
+      'font-style': 'italic'
+    })
+  })
+  $('#btnBold').click(function () {
     $('#specimen-test p').css({
       'font-weight': '700',
-      'font-style': 'normal',
-      });
-  });
-  $('#btnBoldItalic').click(function(){
+      'font-style': 'normal'
+    })
+  })
+  $('#btnBoldItalic').click(function () {
     $('#specimen-test p').css({
       'font-weight': '700',
-      'font-style': 'italic',
-    });
-  });
-});
+      'font-style': 'italic'
+    })
+  })
+})
